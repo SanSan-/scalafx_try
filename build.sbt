faee60abf8a849111e915811653ae637775e18e9
Alex Franz <alexfranz@MacBook-Pro-Alex.local>
2015-11-01 17:04:05 +0300

    modena.css not found was resolved. also ComboBox and ListView were added to Scene

--- build.sbt
+++ build.sbt
@@ -5,3 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.60-R9"
+
+// some hardcode to avoid "modena.css not found" error. later when i will know how to find java_home i will use follow command:
+// unmanagedJars in Compile += Attributed.blank(file(System.getenv("JAVA_HOME") + "/jre/lib/ext/jfxrt.jar"))
+unmanagedJars in Compile += Attributed.blank(file("/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/jre/lib/ext/jfxrt.jar"))
--- src/main/scala/window/package.scala
+++ src/main/scala/window/package.scala
@@ -1,18 +1,27 @@
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.control.Button
+import scalafx.scene.control.{ListView, ComboBox, Button}
 
 /**
- * Created by alexfranz on 01.11.15.
+ * Created by alexfranz
  */
 package object ScalaFxWindow extends JFXApp{
   stage = new PrimaryStage {
     title = "ScalaFX GUI Application"
     scene = new Scene(450,350) {
-      val btn = new Button("Click me!")
-      // this is how to add button to scene
-      content = btn
+      val button = new Button("Click me!")
+      button.layoutX = 100
+      button.layoutY = 50
+      val comboBox = new ComboBox[String](List("Home","About","Contact"))
+      comboBox.layoutX = 200
+      comboBox.layoutY = 50
+      val listView = new ListView[String](List("Item 1", "Item 2", "Item 3", "Item 4"))
+      listView.layoutX = 100
+      listView.layoutY = 100
+      listView.prefHeight = 150
+      // this is how to add all those things to scene
+      content = List(button,comboBox,listView)
     }
   }
 }
